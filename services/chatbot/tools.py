ce139ac38ba652fa86c7bc0190656883c43bf0d1
Muhammad Nur Ichsan <muhammad.ichsan@dataon.com>
2025-06-18 23:37:06 +0700

    update: convert dataframe to mysql connection

--- services/chatbot/tools.py
+++ services/chatbot/tools.py
@@ -1,19 +1,45 @@
 import os
 from pandasai.llm import OpenAI
-from pandasai import SmartDataframe
-import pandas as pd
+from pandasai import SmartDatalake, SmartDataframe
+from pandasai.connectors import MySQLConnector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 openai_llm = OpenAI(
     api_token=os.environ.get("OPENAI_API_KEY"),
 )
 
+def get_tables(table_names):
+    tables = []
+    for table_name in table_names:
+        mysql_connector = MySQLConnector(
+            config={
+                "host": os.environ.get("DB_HOST"),
+                "port": os.environ.get("DB_PORT"),
+                "database": os.environ.get("DB_NAME"),
+                "username": os.environ.get("DB_USERNAME"),
+                "password": os.environ.get("DB_PASSWORD"),
+                "allow_public_key_retrieval": True,
+                "table": table_name,
+            }
+        )
+        sdf = SmartDataframe(mysql_connector)
+        tables.append(sdf)
+    return tables
+
+table_names = os.environ.get("DB_TABLE").split(",")  
+tables = get_tables(table_names)
+lake = SmartDatalake(
+    tables,
+    config={"llm": openai_llm}
+)
+
 def ask_database(question):
-    df = pd.read_csv("data_source.csv")
-    sdf = SmartDataframe(df, config={"llm": openai_llm, "use_duckdb":True})
-    result = sdf.chat(str(question))
+    result = lake.chat(str(question))
     print(question)
     print(result)
-    print(sdf.last_code_generated)
+    print(lake.last_code_generated)
     return result
 
 # Define the function declaration for the model
